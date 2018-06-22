d2f93e7e2b8e60e38d9a5118c497bba8972143c4
mitchelfelske <mitchel.felske@outlook.com>
2018-06-22 17:47:59 -0300

    Refactoring tests to include whole game attribute

--- lib/bowling.rb
+++ lib/bowling.rb
@@ -1,11 +1,26 @@
 class Bowling
   attr_reader :score
+  attr_reader :game
 
   def initialize
     @score = 0
+    @game = Array.new(10) { Hash.new }
   end
 
-  def hit(pin_count)
+  def hit(frame, pin_count)
+    if @game[frame][:roll_one].nil?
+      @game[frame][:roll_one] = pin_count
+    else
+      @game[frame][:roll_two] = pin_count
+    end
     @score += pin_count
+  end
+
+  def frame_score frame
+    @game[frame][:score] = @game[frame][:roll_one] + @game[frame][:roll_two]
+  end
+
+  def frame_score_after_spare(prev_frame, current_frame)
+    @game[prev_frame][:score] += @game[current_frame][:roll_one]
   end
 end
--- spec/bowling_spec.rb
+++ spec/bowling_spec.rb
@@ -1,11 +1,57 @@
 require 'bowling'
 
-RSpec.describe Bowling, '#score' do
-  context "with no strikes or spares" do
+RSpec.describe Bowling do
+
+  before(:each) do
+    @bowling = Bowling.new
+  end
+
+  context "frame with no strike or spare" do
     it "sums the pin count for each roll" do
-      bowling = Bowling.new
-      20.times { bowling.hit(4) }
-      expect(bowling.score).to eq 80
+      @bowling.hit(5,4)
+      @bowling.hit(5,3)
+      @bowling.frame_score 5
+      expect(@bowling.game[5][:roll_one]).to eq 4
+      expect(@bowling.game[5][:roll_two]).to eq 3
+      expect(@bowling.game[5][:score]).to eq 7
     end
   end
+
+  context "previous frame with a spare" do
+    it "sums roll one count to previous frame result" do
+      @bowling.hit(3,4)
+      @bowling.hit(3,6)
+      @bowling.frame_score 3
+
+      @bowling.hit(4,5)
+      @bowling.hit(4,2)
+      @bowling.frame_score 4
+
+      @bowling.frame_score_after_spare(3,4)
+
+      expect(@bowling.game[3][:roll_one]).to eq 4
+      expect(@bowling.game[3][:roll_two]).to eq 6
+      expect(@bowling.game[4][:roll_one]).to eq 5
+      expect(@bowling.game[4][:roll_two]).to eq 2
+      expect(@bowling.game[3][:score]).to eq 15
+      expect(@bowling.game[4][:score]).to eq 7
+    end
+  end
+
+  context "previous frame with a strike" do
+
+    context "current one with no strike or spare" do
+      it "sums both rolls count to previous frame result"
+    end
+
+    context "current one with a spare" do
+      it "sums both rolls count to previous frame result"
+    end
+
+    context "current one with a strike" do
+      it "sums roll one count to previous frame result"
+    end
+
+  end
+
 end
